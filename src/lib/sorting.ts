19ce1ee4d2fcfa2c11ac311dbc09497ca9552906
ju-dev-17 <jahidudd65@gmail.com>
2024-02-02 22:44:17 +0100

    implement selection sort

--- src/lib/sorting.ts
+++ src/lib/sorting.ts
@@ -18,4 +18,24 @@ const bubbleSort = (
   return array;
 };
 
-export { bubbleSort };
+const selectionSort = (
+  array: number[],
+  updateFn: (newState: TData[]) => void
+) => {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+
+    const tmp = array[min];
+    array[min] = array[i];
+    array[i] = tmp;
+    updateFn(array.map((e) => ({ name: e.toString(), uv: e })));
+  }
+};
+
+export { bubbleSort, selectionSort };
