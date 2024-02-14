eabb044120f346a23b1f41a22977acc27b7df352
ju-dev-17 <jahidudd65@gmail.com>
2024-02-14 11:48:41 +0100

    changed description of algorithms

--- src/config/algorithm-list.ts
+++ src/config/algorithm-list.ts
@@ -21,14 +21,14 @@ const algorithmCardList: TAlgorithmCard[] = [
   {
     title: "Bubble Sort",
     description:
-      "Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent pairs and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.",
+      "Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent pairs and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.",
     link: "/visualize/bubble-sort",
     algorithmFn: bubbleSort,
   },
   {
     title: "Selection Sort",
     description:
-      "Selection sort is a straightforward sorting algorithm that works by dividing the input list into two parts: a sorted sublist and an unsorted sublist. It repeatedly selects the smallest (or largest, depending on sorting order) element from the unsorted sublist and swaps it with the leftmost unsorted element. This process continues until the entire list is sorted.",
+      "Selection Sort is a straightforward sorting algorithm that works by dividing the input list into two parts: a sorted sublist and an unsorted sublist. It repeatedly selects the smallest (or largest, depending on sorting order) element from the unsorted sublist and swaps it with the leftmost unsorted element. This process continues until the entire list is sorted.",
     link: "/visualize/selection-sort",
     algorithmFn: selectionSort,
   },
